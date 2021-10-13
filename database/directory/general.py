b2abb913ded7ee7d6f4ca9c6dde8a2450e543783
James Nightingale <james.w.nightingale@durham.ac.uk>
2021-10-13 18:20:03 +0100

    prior id update

---
+++ database/directory/general.py
@@ -0,0 +1,142 @@
+"""
+Feature: Database
+=================
+
+The default behaviour of **PyAutoFit** is for model-fitting results to be output to hard-disc in folders, which are
+straight forward to navigate and manually check. For small model-fitting tasks this is sufficient, however many users
+have a need to perform many model fits to very large datasets, making manual inspection of results time consuming.
+
+PyAutoFit's database feature outputs all model-fitting results as a
+sqlite3 (https://docs.python.org/3/library/sqlite3.html) relational database, such that all results
+can be efficiently loaded into a Jupyter notebook or Python script for inspection, analysis and interpretation. This
+database supports advanced querying, so that specific model-fits (e.g., which fit a certain model or dataset) can be
+loaded.
+
+This example extends our example of fitting a 1D `Gaussian` profile and fits 3 independent datasets each containing a
+1D Gaussian. The results will be written to a `.sqlite` database, which we will load to demonstrate the database.
+
+A full description of PyAutoFit's database tools is provided in the database chapter of the `HowToFit` lectures.
+"""
+# %matplotlib inline
+# from pyprojroot import here
+# workspace_path = str(here())
+# %cd $workspace_path
+# print(f"Working Directory has been set to `{workspace_path}`")
+
+import autofit as af
+
+import os
+from os import path
+import numpy as np
+
+"""
+__Dataset Names__
+
+Load the dataset from hard-disc, set up its `Analysis` class and fit it with a non-linear search. 
+"""
+dataset_name = "gaussian_x1"
+
+"""
+__Model__
+
+Next, we create our model, which again corresponds to a single `Gaussian` with manual priors.
+"""
+model = af.Collection(gaussian=af.ex.Gaussian)
+
+model.gaussian.centre = af.UniformPrior(lower_limit=0.0, upper_limit=100.0)
+model.gaussian.intensity = af.LogUniformPrior(lower_limit=1e-2, upper_limit=1e2)
+model.gaussian.sigma = af.GaussianPrior(
+    mean=10.0, sigma=5.0, lower_limit=0.0, upper_limit=np.inf
+)
+
+"""
+___Session__
+
+To output results directly to the database, we start a session, which includes the name of the database `.sqlite` file
+where results are stored.
+"""
+session = None
+
+"""
+The code below loads the dataset and sets up the Analysis class.
+"""
+dataset_path = path.join("dataset", "example_1d", dataset_name)
+
+data = af.util.numpy_array_from_json(file_path=path.join(dataset_path, "data.json"))
+noise_map = af.util.numpy_array_from_json(
+    file_path=path.join(dataset_path, "noise_map.json")
+)
+
+analysis = af.ex.Analysis(data=data, noise_map=noise_map)
+
+"""
+Resultsare written directly to the `database.sqlite` file omitted hard-disc output entirely, which
+can be important for performing large model-fitting tasks on high performance computing facilities where there
+may be limits on the number of files allowed. The commented out code below shows how one would perform
+direct output to the `.sqlite` file. 
+"""
+dynesty = af.DynestyStatic(
+    name="queries",
+    path_prefix=path.join("database", "directory"),
+    number_of_cores=1,
+    unique_tag=dataset_name,
+    session=session,
+)
+
+result = dynesty.fit(model=model, analysis=analysis)
+
+"""
+__Database__
+
+First, note how the results are not contained in the `output` folder after each search completes. Instead, they are
+contained in the `database.sqlite` file, which we can load using the `Aggregator`.
+"""
+from autofit.database.aggregator import Aggregator
+
+database_file = "database.sqlite"
+
+try:
+    os.remove(path.join("output", database_file))
+except FileNotFoundError:
+    pass
+
+
+agg = Aggregator.from_database(path.join("output", database_file))
+agg.add_directory(directory=path.join("output", "database", "directory"))
+
+"""
+Make sure database + agg can be used.
+"""
+samples_gen = agg.values("samples")
+
+"""
+When we convert this generator to a list and it, the outputs are 3 different MCMCSamples instances. These correspond to 
+the 3 model-fits performed above.
+"""
+path_prefix = agg.search.path_prefix
+agg_query = agg.query(path_prefix == path.join("database", "session", dataset_name))
+print("Total Samples Objects via `path_prefix` model query = ", len(agg_query), "\n")
+
+name = agg.search.name
+agg_query = agg.query(name == "queries")
+print("Total Samples Objects via `name` model query = ", len(agg_query), "\n")
+
+gaussian = agg.model.gaussian
+agg_query = agg.query(gaussian == af.ex.Gaussian)
+print("Total Samples Objects via `Gaussian` model query = ", len(agg_query), "\n")
+
+gaussian = agg.model.gaussian
+agg_query = agg.query(gaussian.sigma > 3.0)
+print("Total Samples Objects In Query `gaussian.sigma < 3.0` = ", len(agg_query), "\n")
+
+gaussian = agg.model.gaussian
+agg_query = agg.query((gaussian == af.ex.Gaussian) & (gaussian.sigma < 3.0))
+print(
+    "Total Samples Objects In Query `Gaussian & sigma < 3.0` = ", len(agg_query), "\n"
+)
+
+unique_tag = agg.search.unique_tag
+agg_query = agg.query(unique_tag == "gaussian_x1_1")
+
+print(agg_query.values("samples"))
+print("Total Samples Objects via unique tag Query = ", len(agg_query), "\n")
---
+++ id_update/update.py
@@ -0,0 +1,33 @@
+"""
+__Imports__
+"""
+import os
+from os import path
+import warnings
+
+warnings.filterwarnings("ignore")
+
+from autoconf import conf
+
+"""
+__Paths__
+"""
+workspace_path = os.getcwd()
+
+config_path = path.join(workspace_path, "config")
+conf.instance.push(new_path=config_path)
+
+"""
+___Database__
+
+The name of the database, which corresponds to the output results folder.
+"""
+output_directory = path.join("id_update", "output", "updated")
+map_filename = path.join("id_update", "update.yaml")
+
+from autofit.tools.update_identifiers import update_identifiers_from_file
+
+update_identifiers_from_file(
+    output_directory=output_directory,
+    map_filename=map_filename
+)
