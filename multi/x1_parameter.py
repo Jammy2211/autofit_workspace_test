a69d7a7c59cb8a7420b11b2d633dea744bc7d5e4
Jammy2211 <james.w.nightingale@durham.ac.uk>
2022-12-24 10:00:46 +0000

    model linking examples

--- multi/x1_parameter.py
+++ multi/x1_parameter.py
@@ -89,3 +89,66 @@ __Result__
 model_data = result_list[0].max_log_likelihood_instance.model_data_1d_via_xvalues_from(
     xvalues=np.arange(data.shape[0])
 )
+
+"""
+__Model Linking 1 (To Instances)__
+
+Compose a model via model linking where some parameters are passed as instances. 
+
+This would traditionally use the model-linking API via something like:
+
+ model = af.Model(af.ex.Gaussian)
+
+ model.sigma = result_list.instance.sigma
+ model.normalization = result_list.instance.normalization
+
+Note that there is slightly different behaviour for sigma and normalizaiton above:
+
+ - The sigma parameter was free across every analysis object in the model fitted above, thus each `model.sigma` value 
+ should be different based on this result.
+ 
+ - The normalization parameter was the same for all analysis objects, so should be the same value for the model.
+"""
+# model_link_2 = NEW API
+
+search = af.DynestyStatic(path_prefix="multi", name="model_linking_2")
+search.fit(model=model_link_2, analysis=analysis)
+
+"""
+__Model Linking 2 (To Model)__
+
+Compose a model via model linking where parameters become model-components treated as `GaussianPriors`. 
+
+This would traditionally use the model-linking API via something like:
+
+ model.sigma = result_list.model.sigma
+ model.normalization = result_list.model.normalization
+
+For sigma, it is straight forward to imagine how it is passed. It had free parameters for both analysis objects
+in the fit above, thus we should pass it as two separate model components.
+
+We have to make a decision about how the model for normalization is passed:
+
+ - The normaliation values were shared in the previous fit, passing it as a model could retain this information and
+   pass it as just 1 free parameter.
+
+ - It could make thee normalization free across both analysis objects, ignoring the parameterization of the fit performed
+   previously.
+ 
+I suggest that ``model`` retains the information of the previous analysis, therefore the simpler model is linked.
+"""
+# model_link_2 = NEW API
+
+search = af.DynestyStatic(path_prefix="multi", name="model_linking_1")
+search.fit(model=model_link_1, analysis=analysis)
+
+"""
+__Model Linking 3 (Relational Models)__
+
+This may require some dedicated thought...
+"""
+# model_link_3 = NEW API
+
+search = af.DynestyStatic(path_prefix="multi", name="model_linking_2")
+search.fit(model=model_link_3, analysis=analysis)
+
